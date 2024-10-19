a67cff0287b3425ecbc7d8f78fe3707d9964e71b
Sahil <sahilarorapersonal2003@gmail.com>
2024-10-19 20:09:34 +0530

    some fixes for trial deployment in contact.tsx

--- frontend/src/Contact.tsx
+++ frontend/src/Contact.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+
+
+// const Contact = () => {
+//   return (
+//     <div className='flex justify-center mt-20'>
+//       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfPkhsQT8Kq5-Al-Uf-f4jmjcRFD_2HtPrzB0a4ebIlGkPyxA/viewform?embedded=true" width="640" height="985" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+//     </div>
+//   )
+// }
+
+// export default Contact
 
 const Contact = () => {
   return (
     <div className='flex justify-center mt-20'>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfPkhsQT8Kq5-Al-Uf-f4jmjcRFD_2HtPrzB0a4ebIlGkPyxA/viewform?embedded=true" width="640" height="985" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfPkhsQT8Kq5-Al-Uf-f4jmjcRFD_2HtPrzB0a4ebIlGkPyxA/viewform?embedded=true"
+        width="640"
+        height="985"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+      >
+        Loading…
+      </iframe>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
